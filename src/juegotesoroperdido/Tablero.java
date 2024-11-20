e55e6cbca6f01be33e3a677ab932c279bd0aa18f
Miguelito <Miguelito@192.168.1.72>
2024-11-20 15:29:55 -0500

    agregar mina te lleve al inicio y borre el rastro

--- src/juegotesoroperdido/Tablero.java
+++ src/juegotesoroperdido/Tablero.java
@@ -12,6 +12,7 @@ public class Tablero {
     private char [][] tablero;
     private boolean[][] minas;
     private int tesoroX, tesoroY;
+    private int inicioX, inicioY; // Coordenadas iniciales del jugador
     private boolean juegoTerminado = false;
 
     public Tablero() {
@@ -27,53 +28,68 @@ public class Tablero {
         }
     }
 
-    // Ubicar al jugador en una posición aleatoria
     public void ubicarJugador(Jugador jugador) {
         Random rand = new Random();
-        jugador.setX(rand.nextInt(SIZE));
-        jugador.setY(rand.nextInt(SIZE));
-        tablero[jugador.getX()][jugador.getY()] = 'J';
+        int x = rand.nextInt(SIZE);
+        int y = rand.nextInt(SIZE);
+
+        jugador.setX(x);
+        jugador.setY(y);
+
+        inicioX = x; // Guardar la posición inicial
+        inicioY = y;
+
+        tablero[x][y] = 'J'; // Marcar al jugador en el tablero
     }
 
-    // Coloca el tesoro formando una "L" de exactamente 10 celdas
     public void ubicarTesoro(Jugador jugador) {
         Random rand = new Random();
         boolean posicionValida = false;
 
-        while (posicionValida== false) {
-            int vertical = rand.nextInt(10); // Número de celdas verticales (0 a 9)
-            int horizontal = 10 - vertical;  // Número de celdas horizontales
+        while (!posicionValida) {
+            int vertical = rand.nextInt(10);
+            int horizontal = 10 - vertical;
             boolean empiezaVertical = rand.nextBoolean();
 
-            if (empiezaVertical == true ) {
+            if (empiezaVertical) {
                 if (jugador.getX() + vertical < SIZE && jugador.getY() + horizontal < SIZE) {
-                    tesoroX = jugador.getX() + vertical;  
-                    tesoroY = jugador.getY() + horizontal;  
+                    tesoroX = jugador.getX() + vertical;
+                    tesoroY = jugador.getY() + horizontal;
                     posicionValida = true;
                 }
             } else {
                 if (jugador.getY() + horizontal < SIZE && jugador.getX() + vertical < SIZE) {
-                    tesoroY = jugador.getY() + horizontal;  
-                    tesoroX = jugador.getX() + vertical;  
+                    tesoroY = jugador.getY() + horizontal;
+                    tesoroX = jugador.getX() + vertical;
                     posicionValida = true;
                 }
             }
         }
 
-        tablero[tesoroX][tesoroY] = 'T';  // Revelar el tesoro para saber si funciona bien :D
+        tablero[tesoroX][tesoroY] = 'T'; // Revelar el tesoro
     }
 
-    
     public void ubicarMinas() {
         Random rand = new Random();
         int minasUbicadas = 0;
         while (minasUbicadas < MINAS) {
             int minaX = rand.nextInt(SIZE);
             int minaY = rand.nextInt(SIZE);
+
             if (!((Math.abs(minaX - tesoroX) <= 1) && (Math.abs(minaY - tesoroY) <= 1)) && tablero[minaX][minaY] == '.') {
                 minas[minaX][minaY] = true;
-                tablero[minaX][minaY] = 'M';  // Revelar las minas para saber si funciona bien :D
+                tablero[minaX][minaY] = 'M'; // Revelar las minas
                 minasUbicadas++;
+            }
+        }
+    }
+
+    private void limpiarRastros() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (tablero[i][j] == '*') {
+                    tablero[i][j] = '.'; // Reemplaza el rastro por una celda vacía
+                }
             }
         }
     }
@@ -88,54 +104,42 @@ public class Tablero {
     }
 
     public void moverJugador(Jugador jugador, char direccion) {
-        tablero[jugador.getX()][jugador.getY()] = '*';  // Marca el rastro del jugador
+        tablero[jugador.getX()][jugador.getY()] = '*'; // Marca el rastro del jugador
         int prevX = jugador.getX();
         int prevY = jugador.getY();
 
         switch (direccion) {
-            case 'w': 
-                 if (jugador.getX() > 0)
-                        jugador.setX(jugador.getX() - 1); break;
-            case 's':
-                    if (jugador.getX() < SIZE - 1) 
-                            jugador.setX(jugador.getX() + 1); break;
-            case 'a': 
-                 if (jugador.getY() > 0) 
-                             jugador.setY(jugador.getY() - 1); break;
-            case 'd': 
-                 if (jugador.getY() < SIZE - 1) 
-                            jugador.setY(jugador.getY() + 1); break;
-            case 'W':
-                    if (jugador.getX() > 0)
-                            jugador.setX(jugador.getX() - 1); break;
-            case 'S': 
-                    if (jugador.getX() < SIZE - 1) 
-                            jugador.setX(jugador.getX() + 1); break;
-            case 'A':
-                    if (jugador.getY() > 0) 
-                            jugador.setY(jugador.getY() - 1); break;
-            case 'D':
-                    if (jugador.getY() < SIZE - 1) 
-                            jugador.setY(jugador.getY() + 1); break;
+            case 'w': if (jugador.getX() > 0) jugador.setX(jugador.getX() - 1); break;
+            case 's': if (jugador.getX() < SIZE - 1) jugador.setX(jugador.getX() + 1); break;
+            case 'a': if (jugador.getY() > 0) jugador.setY(jugador.getY() - 1); break;
+            case 'd': if (jugador.getY() < SIZE - 1) jugador.setY(jugador.getY() + 1); break;
             default: System.out.println("Movimiento no válido"); return;
         }
 
         if (tablero[jugador.getX()][jugador.getY()] == '*') {
-            tablero[prevX][prevY] = '.';  // Limpia el rastro si el jugador retrocede
+            tablero[prevX][prevY] = '.'; // Limpia el rastro si retrocede
         }
         tablero[jugador.getX()][jugador.getY()] = 'J';
     }
 
     public void verificarEstadoJuego(Jugador jugador, Resultado resultado) {
-        if (minas[jugador.getX()][jugador.getY()]==true) {
+        if (minas[jugador.getX()][jugador.getY()] == true) {
             jugador.perderVida();
-            System.out.println("¡Pisaste una mina! Vidas restantes: " + jugador.getVidas());       
-            if (jugador.getVidas() == 0) {
+            System.out.println("¡Pisaste una mina! Vidas restantes: " + jugador.getVidas());
+
+            if (jugador.getVidas() > 0) {
+                limpiarRastros(); // Limpia todos los asteriscos del tablero
+                tablero[jugador.getX()][jugador.getY()] = '.'; // Limpia la celda actual
+                jugador.setX(inicioX);
+                jugador.setY(inicioY);
+                tablero[inicioX][inicioY] = 'J'; // Reaparece en la posición inicial
+                System.out.println("Has regresado a tu posición inicial.");
+            } else {
                 System.out.println("¡Has perdido todas tus vidas!");
                 juegoTerminado = true;
             }
         } else if (jugador.getX() == tesoroX && jugador.getY() == tesoroY) {
-            System.out.println("Felicidades,W Has encontrado el tesoro.");
+            System.out.println("¡Felicidades! Has encontrado el tesoro.");
             juegoTerminado = true;
             resultado.setLlegadaTesoro(true);
         }
@@ -144,6 +148,4 @@ public class Tablero {
     public boolean isJuegoTerminado() {
         return juegoTerminado;
     }
-    
 }
-
